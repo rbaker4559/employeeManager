e54bec892db51e11d9829647bbc288c0a4d3109f
rbaker4559 <reginaldsbaker10@icloud.com>
2024-04-06 13:40:58 -0700

    fixing minor syntax error on schema, adding view role, department, and employee functions to index.js

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
+//imports given requires didn't work
 import inquirer from 'inquirer';
+import mysql2 from 'mysql2';
+
+//mysql2 pool
+const pool = mysql2.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'employee_manager_db'
+})
+
+
+//view all departments function
+function viewDepartments() {
+  pool.query('SELECT * FROM department', (error, results) => {
+    if (error) {
+      console.error('Error retrieving departments: ', error);
+      return;
+    } 
+    console.table(results);
+  })
+};
+
+//view roles function
+function viewRoles() {
+  pool.query('SELECT * FROM role', (error, results) => {
+    if (error) {
+      console.error('Error retrieving roles: ', error);
+      return;
+    } 
+    console.table(results);
+  })
+};
+
+//view employees function
+function viewEmployees() {
+  pool.query('SELECT * FROM employee', (error, results) => {
+    if (error) {
+      console.error('Error retrieving employees: ', error);
+      return;
+    } 
+    console.table(results);
+  })
+};
+
+
+
 
 const questions = [
   {
@@ -13,7 +61,17 @@ const questions = [
 inquirer
   .prompt(questions)
   .then((answers) => {
-    console.log(answers);
+    switch (answers.action) {
+      case 'View All Departments': 
+        viewDepartments();
+        break;
+      case 'View All Roles': 
+        viewRoles();
+        break;
+      case 'View All Employees': 
+        viewEmployees();
+        break;
+    }
   })
   .catch((error) => {
     if (error.isTtyError) {
